8c1381424408f493481c7e4519f9408621c25ec6
zaki-hanafiah <zaki@zakhov.com>
2026-04-10 09:58:00 +0800

    refactor: simplify condition logic

--- next-env.d.ts
+++ next-env.d.ts
@@ -6,3 +6,4 @@ import './.next/dev/types/routes.d.ts'
 // see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
 // __afill_20260430_1540__
 // __afill_20260401_1142__
+// __afill_20260410_958__
